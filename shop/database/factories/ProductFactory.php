423320a35b291f46e973659bb41a1aa61f8a99f5
katide155 <93394912+katide155@users.noreply.github.com>
2022-02-05 09:57:29 +0200

    shop created

---
+++ shop/database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->lexify('????????? product'),
+			'description' => $this->faker->paragraph(15),
+			'price' => $this->faker->randomFloat(2, 0.01, 300),
+			'category_id' => $this->faker->numberBetween(1, 3),
+			'image_url' => $this->faker->imageUrl(),
+        ];
+    }
+}
